9a3f2c63f18c6cf62c2530f54d8eb4484cfae27b
Yousra Lemqaddem <lemqaddem.yousra@gmail.com>
2023-01-12 09:06:16 +0100

    Cart added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,11 @@
+import '../styles/Cart.css'
+
 function Cart() {
 	const monsteraPrice = 8
 	const ivyPrice = 10
 	const flowerPrice = 15
 	return (
-		<div>
+		<div className='lmj-cart'>
 			<h2>Panier</h2>
 			<ul>
 				<li>Monstera : {monsteraPrice}€</li>
@@ -14,4 +16,5 @@ function Cart() {
 		</div>
 	)
 }
+
 export default Cart
